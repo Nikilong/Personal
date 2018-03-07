563b8fda1ec022b8f02a97f1f975fca0acceea0e
Nikilong <247862767@qq.com>
2018-03-08 00:27:09 +0800

    测试脚本

---
+++ 03 虾兽新闻客户端/Classes/Other/Tool/XMtest.m
@@ -0,0 +1,21 @@
+//
+//  XMtest.m
+//  虾兽维度
+//
+//  Created by Niki on 18/3/8.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import "XMtest.h"
+
+@implementation XMtest
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
