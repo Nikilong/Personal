ec898412198b40389188bd1938ba122f6778c66b
Nikilong <247862767@qq.com>
2018-03-08 21:19:52 +0800

    测试提交3

---
+++ 03 虾兽新闻客户端/Classes/Other/Tool/XMewrdcdjd.h
@@ -0,0 +1,13 @@
+//
+//  XMewrdcdjd.h
+//  虾兽维度
+//
+//  Created by Niki on 18/3/8.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XMewrdcdjd : UIView
+
+@end
---
+++ 03 虾兽新闻客户端/Classes/Other/Tool/XMewrdcdjd.m
@@ -0,0 +1,21 @@
+//
+//  XMewrdcdjd.m
+//  虾兽维度
+//
+//  Created by Niki on 18/3/8.
+//  Copyright © 2018年 admin. All rights reserved.
+//
+
+#import "XMewrdcdjd.h"
+
+@implementation XMewrdcdjd
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
