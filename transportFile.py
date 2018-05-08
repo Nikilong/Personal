89b7d0ab62341a3c5c70c92b23fe1fbb84a9a617
Nikilong <247862767@qq.com>
2018-05-08 12:39:50 +0800

    【wifi传输】改善wifi传输的python脚本

--- transportFile.py
+++ transportFile.py
@@ -4,27 +4,47 @@
 import sys,os,time
 from selenium import webdriver
 print '''
-============使用说明============
-传入两个参数:1.上传的网址;2.上传的文件夹
-'''
+    ============使用说明============
+    可选传入2个参数:1.上传的网址;2.上传文件的文件夹路径
+    不传任何参数的话会提示输入上传的url(另外需要在脚本指定资源的路径)
+    传入一个参数:该参数是上传的url(另外需要在脚本指定资源路径)
+    传入两个参数:可以指定上传url和资源路径
+    
+    '''
 global elements         # index.html的网页input元素
 global maxInputCount    # 每次最大上传数量
+global uploadURL        # 上传URL
+global sourcePath       # 原文件路径
 
 # 提供一个函数刷新网页,更新元素
 def refreshUploadhtml():
-    global elements,maxInputCount
+    global elements,maxInputCount,uploadURL
     options = webdriver.ChromeOptions()
     options.set_headless()
     options.add_argument('--disable-gpu')
     browser = webdriver.Chrome(options=options)
-    browser.get(sys.argv[1])
+    browser.get(uploadURL)
     elements = browser.find_elements_by_tag_name('input')
     maxInputCount = len(elements) - 1
 
+#0.根据传入参数进行下一步操作
+agu = len(sys.argv)
+if agu == 1:
+    # 传入上传的网址
+    uploadURL = raw_input('输入上传的URL: ')
+    sourcePath = '/Users/Longcq/Desktop/ToXM'
+elif agu == 2:
+    uploadURL = sys.argv[1]
+    sourcePath = '/Users/Longcq/Desktop/ToXM'
+elif agu == 3:
+    uploadURL = sys.argv[1]
+    sourcePath = sys.argv[2]
+else:
+    print '输入无效'
 
 #1.将要上传的文件传入一个数组
 fileList = []
-for root,dir,files in os.walk(sys.argv[2]):
+for root,dir,files in os.walk(sourcePath):
     for ele in files:
         fileList.append(os.path.join(root,ele))
 
